01e1bb58aecf92e6d765e33bbb2b1de48c61cc3d
Zac Walters <zacwalters4@gmail.com>
2022-08-04 10:26:25 -0600

    Clean up syntax

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,22 +1,26 @@
-var foods = {apples: 23, 
-  grapes: 507, 
-  eggs: 48}
+foods = {
+  apples: 23,
+  grapes: 507,
+  eggs: 48
+}
 
-var person = { height: "6 ft", weight: "160 lbs" }
+person = {
+  height: "6 ft",
+  weight: "160 lbs"
+}
 
-var post = { imageSrc: "./images/beach.png",
+post = {
+  imageSrc: "./images/beach.png",
+  caption: "At the beach with my besties",
+  timestamp: "4:37 PM August 13, 2019",
+  number_likes: 0,
+  comments: []
+}
 
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
-
-
-var post2 =
-{
-      imageSrc: "./images/holiday-party.png",
-      caption: "What a great holiday party omg",
-      timestamp: "11:37 PM December 31, 2019",
-      numberLikes: 13,
-      comments: []
+post2 = {
+  imageSrc: "./images/holiday-party.png",
+  caption: "What a great holiday party omg",
+  timestamp: "11:37 PM December 31, 2019",
+  numberLikes: 13,
+  comments: []
 }
